d787855af5c827d46b2a9f9f896dcb9faf7110bb
OAzab <oaazab10@gmail.com>
2023-12-21 11:12:40 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,60 @@
-'use client'
+"use client";
+import { useEffect, useState } from "react";
+import Cell from "./components/cell";
 
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import Cell from './components/cell';
-
-const WinningCombos = [
-  [0 , 1 , 2]  , [3 , 4 , 5] , [6 , 7 , 8] , [0 , 3 , 6] ,  [1 , 4 , 7] , [2 , 5 , 8] , [0 , 4 , 8] , [2 , 4 , 6]
-]
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 export default function Home() {
-  const [cells , setCells] = useState(["" , "" , "" , "" , "" , "" , "" , "" , ""]);
-  const [go , setGo] = useState("X");
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [go, setGo] = useState("circle");
   const [winningMessage, setWinningMessage] = useState("");
 
-  
   useEffect(() => {
-    WinningCombos.forEach((combo) => {
-      const Xwins = combo.every((cell => cells[cell]=== "X"))
-      const Circlewins = combo.every((cell => cells[cell]=== "circle"))
-      
-      if(Xwins) {
-        setWinningMessage("🎉 X Wins! 🎉")
-      } else if(Circlewins) {
-        setWinningMessage("🎉 Circle Wins! 🎉")
+    winningCombos.forEach((combo) => {
+      const circleWins = combo.every((cell) => cells[cell] === "circle");
+      const crossWins = combo.every((cell) => cells[cell] === "cross");
+
+      if (circleWins) {
+        setWinningMessage("Circle Wins!");
+      } else if (crossWins) {
+        setWinningMessage("Cross Wins!");
       }
-    })
-  }, [cells])
-
+    });
+  }, [cells, winningMessage]);
 
   useEffect(() => {
-    if(cells.every((cell => cell !== "")) && !winningMessage) {
-      setWinningMessage("Draw,🤝 Refresh the page to restart.")
+    if (cells.every((cell) => cell !== "") && !winningMessage) {
+      setWinningMessage("Draw!");
     }
-
-  }, [cells , winningMessage])
+  }, [cells, winningMessage]);
 
   return (
-    <main className='main-app'>
-     <div className="gameBoard">
-      {cells.map((cell , index) => (
-        <Cell id={index} go={go}
-         setGo={setGo} 
-         key={index}
-          cells={cells} 
-          setCells={setCells} 
-          cell={cell}
-          winningMessage={winningMessage} />
-      ))}
-     </div>
-     <h1>{winningMessage}</h1>
-     {!winningMessage && <h1>{`It's ${go} Turn.`}</h1>}
-    </main>
-  )
+    <div className="container">
+      <div className="gameboard">
+        {cells.map((cell, index) => (
+          <Cell
+            id={index}
+            go={go}
+            setGo={setGo}
+            key={index}
+            cells={cells}
+            setCells={setCells}
+            cell={cell}
+            winningMessage={winningMessage}
+          />
+        ))}
+      </div>
+      <div>{winningMessage}</div>
+      {!winningMessage && <div>{`its now ${go} turn!`}</div>}
+    </div>
+  );
 }
